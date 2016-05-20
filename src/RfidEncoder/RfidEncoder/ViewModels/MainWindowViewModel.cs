af3b1493bc975dee178d1c07e21b59dd33b226ac
trang1 <trang@bk.ru>
2016-05-20 17:15:50 +0300

    connect

--- src/RfidEncoder/RfidEncoder/ViewModels/MainWindowViewModel.cs
+++ src/RfidEncoder/RfidEncoder/ViewModels/MainWindowViewModel.cs
@@ -48,8 +48,30 @@ namespace RfidEncoder.ViewModels
         }
 
         public ComPortInfo SelectedComPort { get; set; }
+        public IList<string> Regions { get; set; }
 
+        public string SelectedRegion { get; set; }
+        public IList<object> BaudRates {
+            get
+            {
+                return new List<object>
+                {
+                    new {ID = 0, Title = "Select"},
+                    new {ID = 1, Title = "9600"},
+                    new {ID = 2, Title = "19200"},
+                    new {ID = 3, Title = "38400"},
+                    new {ID = 4, Title = "115200"},
+                    new {ID = 5, Title = "230400"},
+                    new {ID = 6, Title = "460800"},
+                    new {ID = 7, Title = "921600"}
+                };
+            }}
+
+        public string SelectedBaudRate { get; set; }
         public ICommand ConnectCommand { get; set; }
+
+        public bool IsConnected { get; set; }
+
 
         #endregion
     }
